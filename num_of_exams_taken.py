a0de3f01bc9f9602201a0314c9124b1f6f91013a
QuangNguyen <ducquang0301@gmail.com>
2020-11-25 11:02:10 +0700

    add piechart

--- num_of_exams_taken.py
+++ num_of_exams_taken.py
@@ -11,6 +11,9 @@ header = header.split(",")
 #take subjects from header
 subjects = header[5:]
 
+total_subjects = subjects[0:2] + subjects[4:]
+
+
 #get students list
 students = data[1:]
 students.pop()
@@ -29,6 +32,30 @@ for s in students:
 			if i == 7 or i == 8:
 				count -= 1
 	num_of_exams_taken[count] += 1
+
+total = 0
+for i in num_of_exams_taken:
+	total += i
+
+num_of_exams_taken_per = [0,0,0,0,0,0,0,0,0,0]
+for i in range(len(num_of_exams_taken)):
+	num_of_exams_taken_per[i] = round(num_of_exams_taken[i]*100/total,4)
+
+
+
+#Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = num_of_exams_taken
+sizes = num_of_exams_taken_per
+explode = (0, 0, 0, 0, 0, 0, 0.1, 0, 0, 0)
+#explode = (0,0,0,0,0,0,0,0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()
+
 
 print(subjects)
 print(students[0])
