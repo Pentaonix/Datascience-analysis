7bfdd436e3f28f3e968729d461ede8e33cdedaa5
Pentaonix <ducquang0301@gmail.com>
2021-01-28 15:02:15 +0700

    done chart

--- scores_average_by_exams_taken.py
+++ scores_average_by_exams_taken.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 
-with open("clean_data.csv", mode = "r") as file:
+with open("clean_data.csv",encoding="utf8", mode = "r") as file:
 	data = file.read().split("\n")
 
 #take header
@@ -13,12 +13,12 @@ subjects = header[5:]
 
 total_subjects = subjects[0:2] + subjects[4:]
 
-
 #get students list
 students = data[1:]
 students.pop()
 total_students = len(students)
 
+label = ["0 mon", "1 mon", "2 mon", "3 mon", "4 mon", "5 mon", "6 mon", "7 mon", "8 mon", "9 mon"]
 num_of_exams_taken = [0,0,0,0,0,0,0,0,0,0]
 num_of_exams_taken_per = [0,0,0,0,0,0,0,0,0,0]
 scores_sum_by_exems_taken = [0,0,0,0,0,0,0,0,0,0]
@@ -59,32 +59,32 @@ for i in range(len(scores_sum_by_exems_taken)):
 print(num_of_exams_taken)
 print(scores_average_by_exems_taken)
 
-# #barchart
-# #set limit to vertical axis
-# figure, axis = plt.subplots()
-# axis.set_ylim(0,100)
+#barchart
+#set limit to vertical axis
+figure, axis = plt.subplots()
+axis.set_ylim(0,10)
 
-# #list from 0-11 -> get name
-# y_pos = np.arange(len(subjects))
+#list from 0-11 -> get name
+y_pos = np.arange(len(label))
 
-# #plot the barchart using 2 list(name,value)
-# plt.bar(y_pos, dont_take_exam_percentage, align='center', alpha=0.5)
+#plot the barchart using 2 list(name,value)
+plt.bar(y_pos, scores_average_by_exems_taken, align='center', alpha=0.5)
 
-# #change horizontal category name
-# plt.xticks(y_pos, subjects)
+#change horizontal category name
+plt.xticks(y_pos, label)
 
-# #labeling for vertical axis
-# plt.ylabel('Percentage(%)')
+#labeling for vertical axis
+plt.ylabel('Scores')
 
-# #barchart's name
-# plt.title('Exams_not_taken')
+#barchart's name
+plt.title('Scores_average_by_exams_taken')
 
-# #set labels for columns
-# rects = axis.patches
-# for rect, label in zip(rects, dont_take_exam):
-#     height = rect.get_height()
-#     axis.text(rect.get_x() + rect.get_width() / 2, height + 1, label,
-#             ha='center', va='bottom')
+#set labels for columns
+rects = axis.patches
+for rect, label in zip(rects, scores_average_by_exems_taken):
+    height = rect.get_height()
+    axis.text(rect.get_x() + rect.get_width() / 2, height , label,
+            ha='center', va='bottom')
 
-# plt.show()
+plt.show()
 
